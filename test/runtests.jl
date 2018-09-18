69749af84c12ee7403376ba90484fd4b304bf27d
Luca Brugnolini <luca.brugnolini@gmail.com>
2018-09-18 09:13:17 +0200

    testing factotum

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,18 @@
 using Factotum
 using Base.Test
-
+using RCall
 # write your own tests here
-@test 1 == 2
+
+x = rand(100,10)
+f_julia = Factotum.FactorModel(x)
+
+R"  
+x <- $x
+f <- princomp(x)
+var <- summary(f)"
+
+f_r = reval("f")
+var_r = reval("var")
+
+@test isapprox(convert(Array,var_r[1]), f_julia.sdev)
+@test isapprox(convert(Array,f_r[6]), f_julia.factors)
