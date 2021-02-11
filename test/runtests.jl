53b52647c34ced15ca1e22c6194d165f95a88e6f
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2021-02-11 13:15:29 +0100

    Improved Information Criteria

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,5 @@
-using Factotum, Random, Statistics, LinearAlgebra, Test
-T, n, r = (100, 10, 6)
-Random.seed!(101020)
+using Factotum, Statistics, LinearAlgebra, Test
+T, n, r = (200, 10, 6)
 x = rand(T,n)                  # generate data
 fm = Factotum.FactorModel(x, 6; scale = true)      # fit factor model
 
@@ -14,21 +13,23 @@ F = Factotum.factors(fm)
 @test Σ ≈ diagm(0=>σ.^2)
 @test Λ'Λ/n ≈ diagm(0=>ones(r))
 
+
+## Replicate Ng and Bai (2002)
+function simulate_factormodel(r, T, N)
+    F = randn(T,r)
+    Λ = randn(r,N)
+    e = sqrt(r).*randn(T,N)
+    F*Λ .+ e 
+end
+
+X = simulate_factormodel(1, 251, 78)
+fm = Factotum.FactorModel(X, 10; scale = true)
+Factotum.informationcriteria((IC1, IC2), fm, 10)
+
+X = simulate_factormodel(3, 100, 20)
+fm = Factotum.FactorModel(X, 10; scale = true)
+ic = Factotum.informationcriteria((IC1, IC2), fm, 10)
+
+findmin(ic)
 ## Test the proportion of the variance explained
 
-## Test
-
-####### Call R, start ################################################
-R"
-x <- $x
-f <- princomp(x)
-var <- summary(f)"
-####### Call R, end ###################################################
-
-f_r = reval("f")     # export R factor model in julia workspace
-var_r = reval("var") # export R factor variance in julia workspace
-
-@test isapprox(convert(Array,var_r[1]), f_julia.sdev)   # testing variances
-@test isapprox(convert(Array,f_r[2]), f_julia.loadings) # testing loadings
-@test isapprox(convert(Array,f_r[3]), f_julia.loadings) # testing loadings
-@test isapprox(convert(Array,f_r[6]), f_julia.factors)  # testing factors
