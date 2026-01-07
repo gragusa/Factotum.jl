10c4cf3bcb98212160dfd1087ab7a92835d710e6
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2026-01-07 14:44:53 +0000

    fixed

--- test/test_sw.jl
+++ test/test_sw.jl
@@ -8,10 +8,10 @@
     ftm = Factotum.FactorModel(X_full, 6; scale=true, method=:ls)
 
     expected_factors = [
-        1.8682   -0.3544   -0.2306    0.0071   -1.7681   -0.9233
-        4.4440   -0.1107    0.1187    0.2745    0.2588   -0.8757
-        2.6460   -0.0584   -1.5195   -0.5259    0.6978   -1.1627
-        2.4535   -0.6135   -6.6660   -1.2342    0.2477   -1.4134
+        -1.8682   -0.3544   -0.2306   -0.0071    1.7681   -0.9233
+        -4.4440   -0.1107    0.1187   -0.2745   -0.2588   -0.8757
+        -2.6460   -0.0584   -1.5195    0.5259   -0.6978   -1.1627
+        -2.4535   -0.6135   -6.6660    1.2342   -0.2477   -1.4134
     ]
 
     expected_loadings = [
@@ -22,7 +22,7 @@
     ]
 
     @testset "Factor values (last 4 rows)" begin
-        @test ftm.factor[end-3:end, :] ≈ expected_factors atol=1e-3
+        @test ftm.factors[end-3:end, :] ≈ expected_factors atol=1e-3
     end
 
     @testset "Loading values (last 4 rows)" begin
